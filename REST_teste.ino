b0a377a3101845d2411cfa7ae3c8dae4982fb6d3
Giovanni Vinturini <go_vinturini@hotmail.com>
2019-02-27 22:23:49 -0300

    Criação da primeira versão apenas com teste REST

---
+++ REST_teste.ino
@@ -0,0 +1,41 @@
+#include "WiFi.h"
+#include "aREST.h"
+
+aREST rest = aREST();
+
+WiFiServer server(80);
+
+const char* ssid = "Giovini";
+const char* password = "navi2517";
+
+int testFunction(String command){
+  Serial.println("Request REST recebido");
+}
+
+void setup() {
+  Serial.begin(115200);
+  rest.function("test", testFunction);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi conectado com IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (client){
+    while (!client.available()){
+      delay(5);
+    }
+    rest.handle(client);
+  }
+
+}
